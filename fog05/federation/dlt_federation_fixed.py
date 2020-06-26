2b1b2b3c1de39a1ef1305c10c9e45d20051ed624
kirilantevski <kirilantevski@gmail.com>
2020-06-26 16:16:54 +0200

    .

--- fog05/federation/dlt_federation_fixed.py
+++ fog05/federation/dlt_federation_fixed.py
@@ -109,8 +109,8 @@ def packNetData(net_info):
         net["uuid_1"] = uuid[0] + "-" + uuid[1] + "-" + uuid[2]
         net["uuid_2"] = uuid[3] + "-" + uuid[4]
     net['name'] = net_info['name']+';'+net_info['net_type']+';'+str(net_info['port'])+';'+str(net_info['vni'])
-    net_name_bytes = Web3.toBytes(text= net['name'])
-    print("Packed OK") if Web3.is_encodable(_type= 'bytes32', value= net_name_bytes) else print("Packing failed!")
+    net_name_bytes = web3.toBytes(text= net['name'])
+    print("Packed OK") if web3.is_encodable(_type= 'bytes32', value= net_name_bytes) else print("Packing failed!")
     net['net_type'] = net_info['mcast_addr']
     net['is_mgmt'] = net_info['is_mgmt']
     return net
