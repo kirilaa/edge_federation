bb56f9bb1f691acef74d6905073e46542bd7c9d4
kirilantevski <kirilantevski@gmail.com>
2020-07-02 19:30:33 +0200

    blockchain reset

--- fog05/federation/dlt_federation_fixed.py
+++ fog05/federation/dlt_federation_fixed.py
@@ -19,7 +19,7 @@ with open(abi_path+"Federation.json") as c_json:
     contract_json = json.load(c_json)
 
 contract_abi = contract_json["abi"]
-contract_address = Web3.toChecksumAddress('0xc76d33f788a17fe014b9E8D00505FA51F9804f97')
+contract_address = Web3.toChecksumAddress('0x2759908C71342E276E2B5588C441E2c35524e575')
 
 Federation_contract = web3.eth.contract(abi= contract_abi, address = contract_address)
 
