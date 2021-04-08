4e03f2aeddd7c680b6984b30fb429b1132deabe9
kirilantevski <kirilantevski@gmail.com>
2021-04-08 18:16:20 +0200

    fix: vni port removed from packing data

--- fog05/federation/eth_federation/authority_eth_federation.py
+++ fog05/federation/eth_federation/authority_eth_federation.py
@@ -329,7 +329,7 @@ def packNetData(net_info):
     if len(uuid)< 6:
         net["uuid_1"] = uuid[0] + "-" + uuid[1] + "-" + uuid[2]
         net["uuid_2"] = uuid[3] + "-" + uuid[4]
-    net['name'] = net_info['name']+';'+net_info['net_type']+';'+str(net_info['port']))
+    net['name'] = net_info['name']+';'+net_info['net_type']+';'+str(net_info['port'])
     net_name_bytes = web3.toBytes(text= net['name'])
     print("Packed OK") if web3.is_encodable(_type= 'bytes32', value= net_name_bytes) else print("Packing failed!")
     net['net_type'] = net_info['mcast_addr']
