47580d2f9a0520af4a7c9d25bdf41e688ec2e127
kirilantevski <kirilantevski@gmail.com>
2021-04-08 14:27:11 +0200

    fix: node IDs changed in the file

--- fog05/federation/cosmos_federation/tender_federation.py
+++ fog05/federation/cosmos_federation/tender_federation.py
@@ -21,8 +21,8 @@ descs_d1 = ['gw.json','radius.json','ap1.json']
 descs_d2 = ['ap2.json']
 
 d1_n1 = 'dc02633d-491b-40b3-83be-072748142fc4' #fog02
-d1_n2 = 'c9f23aef-c745-4f58-bd59-3603fc1721b6' #fog03
-d2_n1 = '1e03d6b9-908e-44e6-9fc2-3282e38c442d' #fog01
+d1_n2 = '1e03d6b9-908e-44e6-9fc2-3282e38c442d' #fog01
+d2_n1 = 'c9f23aef-c745-4f58-bd59-3603fc1721b6' #fog03
 
 
 
