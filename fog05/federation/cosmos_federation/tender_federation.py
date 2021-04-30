197612d4a334f2653909d61ee3e914b069c8c5cd
kirilantevski <kirilantevski@gmail.com>
2021-04-30 14:53:40 +0200

    fix

--- fog05/federation/cosmos_federation/tender_federation.py
+++ fog05/federation/cosmos_federation/tender_federation.py
@@ -630,7 +630,7 @@ def provider(fog_05, host_id, ip_addr):
     if isLosingDomain(host_id):
         print("LOOSER DOMAIN.... waiting 5 seconds")
         time.sleep(5)
-        return False
+        # return False
     if not writeChain(placeBid_key, placeBid_value):
         placeBid_value = str(placeBid_value)
     print("Bid placed")
