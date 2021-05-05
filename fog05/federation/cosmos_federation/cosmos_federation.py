7a868f1107d58fd12466c0bfc52cb3f395d87303
kirilantevski <kirilantevski@gmail.com>
2021-05-05 16:33:25 +0200

    fix

--- fog05/federation/cosmos_federation/cosmos_federation.py
+++ fog05/federation/cosmos_federation/cosmos_federation.py
@@ -650,7 +650,7 @@ def provider(fog_05, host_id):
     print("Bid placed")
     if isLosingDomain(host_id):
         print("LOOSER DOMAIN.... waiting 5 seconds")
-        time.sleep(5)
+        time.sleep(3)
         # return False
     sendTransaction(new_bid_full_string)
     measure("BidIPsent") 
